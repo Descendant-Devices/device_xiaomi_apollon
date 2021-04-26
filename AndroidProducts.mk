801610eae43c9d681c5e806f2cee99609cd6b748
Ramii Ahmed <ramy@ahmedramy.com>
2021-04-26 22:14:28 +0000

    apollon: adapt to Descendant

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollon.mk
+    $(LOCAL_DIR)/descendant_apollon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollon-user \
-    lineage_apollon-userdebug \
-    lineage_apollon-eng
+    descendant_apollon-user \
+    descendant_apollon-userdebug \
+    descendant_apollon-eng
--- descendant_apollon.mk
+++ descendant_apollon.mk
@@ -8,21 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from apollon device
 $(call inherit-product, device/xiaomi/apollon/device.mk)
 
-PRODUCT_NAME := lineage_apollon
+# Inherit some common Descendant stuff.
+$(call inherit-product, vendor/descendant/config/common_full_phone.mk)
+
+PRODUCT_NAME := descendant_apollon
 PRODUCT_DEVICE := apollon
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Mi 10T
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="apollo-user 11 RKQ1.200826.002 V12.1.3.0.RJDMIXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/apollo_global/apollo:11/RKQ1.200826.002/V12.1.3.0.RJDMIXM:user/release-keys
