30d3ed84d2ed142ee2fd0e575b0999f2778c22c8
Ramii Ahmed <ramy@ahmedramy.com>
2021-11-06 10:35:32 +0100

    apollon: adapt to Descendant

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_apollon.mk
+    $(LOCAL_DIR)/aosp_apollon.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_apollon-user \
-    fluid_apollon-userdebug \
-    fluid_apollon-eng
+    aosp_apollon-user \
+    aosp_apollon-userdebug \
+    aosp_apollon-eng
