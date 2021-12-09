ac415f8d7c1cd0bbc87de2fae30f5808da47aec6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-10 00:37:47 +0100

    apollon: Update blobs from V12.5.7.0.RJDMIXM
    Change-Id: I32673211fbba25e56e3052d125054127ea9b5c45

--- libinit/init_xiaomi_apollon.cpp
+++ libinit/init_xiaomi_apollon.cpp
@@ -21,7 +21,7 @@ static const variant_info_t apollocn_info = {
     .nfc = true,
 };
 
-static const variant_info_t apollo_info = {
+static const variant_info_t apollo_global_info = {
     .hwc_value = "",
     .sku_value = "std",
 
@@ -45,10 +45,23 @@ static const variant_info_t apollopro_info = {
     .nfc = true,
 };
 
+static const variant_info_t apollo_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "apollo",
+    .marketname = "Redmi K30S Ultra",
+    .model = "M2007J3SC",
+
+    .nfc = true,
+};
+
 static const std::vector<variant_info_t> variants = {
     apollocn_info,
-    apollo_info,
+    apollo_global_info,
     apollopro_info,
+    apollo_info,
 };
 
 void vendor_load_properties() {
