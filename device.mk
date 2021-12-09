882ecb5067dd5e186ed2785c64f59382469e10aa
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 20:24:28 +0100

    apollon: Namespace the tree
    Change-Id: I4642cbdff50e5f43e35dffb726565a829a8ccada

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from firmware repo
 $(call inherit-product, vendor/xiaomi-firmware/apollon/firmware.mk)
 
