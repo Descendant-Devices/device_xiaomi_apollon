63b2719510e0d4cb472ee4832248a53efabe30b1
jjpprrrr <zhongcy95@gmail.com>
2021-12-26 17:34:43 +0100

    apollon: use AOSP recovery
    Change-Id: Ibf4c810954ea23d542897f7eec536ae4cf575207

--- aosp_apollon.mk
+++ aosp_apollon.mk
@@ -15,6 +15,7 @@ $(call inherit-product, device/xiaomi/apollon/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_NAME := aosp_apollon
 PRODUCT_DEVICE := apollon
